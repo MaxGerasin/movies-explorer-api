2e9ae631982ecb93c2f47b844926c1e5cf8afb63
MaxGerasin <maxgerasin32@gmail.com>
2023-09-12 20:00:12 +0300

    Update movies.js

--- controllers/movies.js
+++ controllers/movies.js
@@ -51,14 +51,14 @@ const postMovie = (req, res, next) => {
 };
 
 const deleteMovie = (req, res, next) => {
-  Movie.findOne({ movieId: req.params._id, owner: req.user._id })
+  Movie.findById(req.params._id)
     .then((movie) => {
       if (movie) {
         const owner = movie.owner.toString();
         if (owner !== req.user._id) {
           throw new ForbiddenError(errorMessages.FORBIDDEN);
         } else {
-          return Movie.findOneAndRemove({ movieId: req.params._id });
+          return Movie.findByIdAndRemove(req.params._id);
         }
       } else {
         throw new NotFoundDataError(errorMessages.NOT_FOUND_DATA);
